420feffc011c9c4c5a8612a06132d85c64a69282
Lucie Danard <lucie.danard@gmail.com>
2021-09-22 14:30:25 +0200

    start numerologie

---
+++ numerologie.js
@@ -0,0 +1,58 @@
+function nombre(chaine) {
+    var somme = 0
+    for (var i=0; i < chaine.length; i++) {
+        somme += chaine.charCodeAt(i)
+    }
+    return somme
+}
+
+function somme(nombre) {
+    var somme = 0
+    chaine = String(nombre)
+    for (var i=0; i < chaine.length ; i++) {
+        somme += parseInt(chaine.charAt(i))
+    }
+    return somme
+}
+
+function chiffreAssocie(chaine) {
+    valeur = nombre(chaine)
+
+    while (valeur > 9) {
+        valeur = somme(valeur)
+    }
+    return valeur
+}
+
+// // test de nombre(chaine)
+
+// // est-ce 2x plus ?
+// console.log(nombre("cou"))
+// console.log(nombre("coucou"))
+
+// // chaque caractère :la somme est-elle correcte ?
+// for (c of "cou") { 
+//     console.log(c + " : " + nombre(c))
+// }
+// // fin de test de nombre(chaine)
+
+// // test de somme(nombre)
+// console.log(somme(132))
+
+// // avec un chiffre : charAt != charCodeAt
+// console.log(somme(4))
+// console.log("4".charCodeAt(0))
+// console.log("4".charAt(0))
+
+// // conversion chaine de caracteres et nombre
+// console.log(typeof "4".charAt(0))
+// console.log(parseInt("4".charAt(0)))
+// console.log(typeof parseInt("4".charAt(0)))
+// // fin de test de somme(nombre)
+
+// // test de chiffreAssocie(chaine)
+
+// //test valeur somme des chiffres
+// console.log(nombre("coucou"))
+// console.log(chiffreAssocie("coucou"))
+// // fin de test de chiffreAssocie(chaine)
